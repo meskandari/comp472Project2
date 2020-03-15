857dd51d729474713c82143397dfa237865b050a
Martin <martypolish@gmail.com>
2020-03-15 13:17:12 -0400

    3/4 of model architecture completed

--- Comp472project2.py
+++ Comp472project2.py
@@ -28,35 +28,176 @@ class Model:
     # default constructor
     def __init__(self):
       self.vocabular = getVocabulary()
-      self.ngram = None
-      self.smoothing = None
-      self.trainingFile = None
-      self.testingFile = None
+      self.ngram = getNgram()
+      self.smoothing = getSmoothing()
+      self.trainingFile = getTrainingFile()
+      self.testingFile = getTestFile()
 
+    #parameterized constructor
     def __init__(self,vocabulary,ngram,smoothing=0,trainingFile,testingFile):
-        self.vocabulary = vocabulary
-        self.ngram = ngram
-        self.smoothing = smoothing
-        self.trainingFile = trainingFile
-        self.testingFile = testingFile
+        self.vocabulary = getVocabulary(vocabulary)
+        self.ngram = getNgram(ngram)
+        self.smoothing = getSmoothing(smoothing)
+        self.trainingFile = getTrainingFile(trainingFile)
+        self.testingFile = getTestFile(testingFile)
 
     def getVocabulary(self,vocabulary=-1):
 
         choice = vocabulary
+
         if(choice==-1):
             print("Select a number for which vocabulary you would like to use:")
-            print("1 : Fold the corpus to lowercase and use only the 26 letters of the alphabet [a-z]")
-            print("2 : Distinguish up and low cases and use only the 26 letters of the alphabet [a-z,A-Z]")
+            print("0 : Fold the corpus to lowercase and use only the 26 letters of the alphabet [a-z]")
+            print("1 : Distinguish up and low cases and use only the 26 letters of the alphabet [a-z,A-Z]")
             print("2 : Distinguish up and low cases and use all characters accepted by the built-in isalpha() method")
             choice = input ("Enter your choice: ")
 
         switcher = {
             0: [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x,y,z],
-            1: [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+            1: [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z],
             2: [T,B,D]
             }
         
         return switcher.get(choice,"Invalid selection")
+
+    def getNgram(self,ngram=-1):
+
+        choice = ngram
+
+        if(choice==-1):
+            print("Select a number for which size n-gram you would like to use:")
+            print("1 : character unigrams")
+            print("2 : character bigrams")
+            print("3 : character trigrams")
+            choice = input ("Enter your choice: ")
+
+        switcher = {
+            1: 1,
+            2: 2,
+            3: 3
+            }
+        
+        return switcher.get(choice,"Invalid selection")
+
+        def getSmoothing(self,smoothing=0):
+
+            choice = smoothing
+
+            if(choice==0):
+
+                interrupt = False
+                count = 0
+                
+                while(!interrupt):
+                    
+                    count = count + 1
+                    
+                    choice = input ("Enter a smoothing value between 0 and 1 : ")
+                    
+                    if(choice>=0 or choice<=1):
+                        interrupt = True
+                    
+                    if(count>3):
+                    print("You failed to provide a smoothing value between 0 and 1, program will continue with default value: 0 ")
+                    choice==0
+                    interrupt = True
+        
+            return choice
+
+        def getTrainingFile(self,trainingFile=""):
+
+            filename = trainingFile
+            count = 0
+
+            try:
+            # read the data into a list
+                with open(str(fileName)) as file:
+                    dataSet = file.readlines()
+
+            except FileNotFoundError :
+                print("File does not exist")
+                filename=""
+
+            if(filename==""):
+
+                interrupt = False
+
+                while(!interrupt):
+
+                    filename = input ("Enter a valid TRAINIGN file name with the extension : ")
+                
+                    try:
+                        # read the data into a list
+                        with open(str(fileName)) as file:
+                            dataSet = file.readlines()
+
+                    except FileNotFoundError :
+                        print("File does not exist")
+
+                
+                    if(len(dataSet)>0):
+                        interrupt = True
+                    
+                    if(count>3):
+                    print("You failed to provide a valid TRAINING file, program will use default training dataset")
+                    
+                    filename = "training-tweets.txt"
+
+                     # read the data into a list
+                    with open(str(fileName)) as file:
+                        dataSet = file.readlines()
+
+                    interrupt = True
+
+            return dataSet
+
+
+        def getTestFile(self,testFile=""):
+
+            filename = testFile
+            count = 0
+
+            try:
+            # read the data into a list
+                with open(str(fileName)) as file:
+                    dataSet = file.readlines()
+
+            except FileNotFoundError :
+                print("File does not exist")
+                filename=""
+
+            if(filename==""):
+
+                interrupt = False
+
+                while(!interrupt):
+
+                    filename = input ("Enter a valid TEST file name with the extension : ")
+                
+                    try:
+                        # read the data into a list
+                        with open(str(fileName)) as file:
+                            dataSet = file.readlines()
+
+                    except FileNotFoundError :
+                        print("File does not exist")
+
+                
+                    if(len(dataSet)>0):
+                        interrupt = True
+                    
+                    if(count>3):
+                    print("You failed to provide a valid TEST file, program will use default training dataset")
+                    
+                    filename = "test-tweets-given.txt"
+
+                     # read the data into a list
+                    with open(str(fileName)) as file:
+                        dataSet = file.readlines()
+
+                    interrupt = True
+
+            return dataSet
 
     
     
