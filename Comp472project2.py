b8f67b60b777729dafbdd3f12e0810ead91ca532
Martin <martypolish@gmail.com>
2020-03-15 12:44:33 -0400

    added getVocabulary to Model class

--- Comp472project2.py
+++ Comp472project2.py
@@ -22,3 +22,41 @@ class Language(Enum):
     ES = 3 #SPANISH
     EN = 4 #ENGLISH
     PT = 5 #PORTUGUESE
+
+class Model:
+    
+    # default constructor
+    def __init__(self):
+      self.vocabular = getVocabulary()
+      self.ngram = None
+      self.smoothing = None
+      self.trainingFile = None
+      self.testingFile = None
+
+    def __init__(self,vocabulary,ngram,smoothing=0,trainingFile,testingFile):
+        self.vocabulary = vocabulary
+        self.ngram = ngram
+        self.smoothing = smoothing
+        self.trainingFile = trainingFile
+        self.testingFile = testingFile
+
+    def getVocabulary(self,vocabulary=-1):
+
+        choice = vocabulary
+        if(choice==-1):
+            print("Select a number for which vocabulary you would like to use:")
+            print("1 : Fold the corpus to lowercase and use only the 26 letters of the alphabet [a-z]")
+            print("2 : Distinguish up and low cases and use only the 26 letters of the alphabet [a-z,A-Z]")
+            print("2 : Distinguish up and low cases and use all characters accepted by the built-in isalpha() method")
+            choice = input ("Enter your choice: ")
+
+        switcher = {
+            0: [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x,y,z],
+            1: [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+            2: [T,B,D]
+            }
+        
+        return switcher.get(choice,"Invalid selection")
+
+    
+    
